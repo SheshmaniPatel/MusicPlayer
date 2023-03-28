0934a56e2041c86bb7316eb4431988eada474814
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-03-28 16:39:38 +0530

    Music Player Project

---
+++ script.js
@@ -0,0 +1,19 @@
+// var i=10 ; 
+//    do { 
+//        console.log("hii"); 
+//      } while(i<5);
+//      console.log(null == undefined);
+// console.log(typeof(null) == typeof(undefined));
+// console.log(null === undefined);
+// console.log( typeof( typeof( 100 ) ) );
+var a;
+if( typeof(a) ) {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+switch(expression)
+{
+    statements
+}
